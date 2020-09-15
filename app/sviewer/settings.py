96b114f62331d43318e809f88f362e298ebe74c4
root <root@Defiance.shizawa.mech.keio.ac.jp>
2020-09-15 22:24:53 +0900

    [general] Revise nginx settings (Handling of static and media files) for release build

--- app/sviewer/settings.py
+++ app/sviewer/settings.py
@@ -135,12 +135,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles/")
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static/"),
+)
+
+MEDIA_URL = '/media/'
 if DEBUG:
-    STATICFILES_DIRS = (
-        os.path.join(BASE_DIR, "static"),
-    )
+    MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 else:
-    STATIC_ROOT = os.path.join(BASE_DIR, "static")
+    MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles/')
 
 ASGI_APPLICATION = 'sviewer.routing.application'
 
